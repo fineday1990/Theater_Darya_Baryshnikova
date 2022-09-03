0591d108b5b0459e37d921bf16818d65dad8bdb0
ilya <vishnyajudo@gmail.com>
2022-09-03 10:43:15 +0300

    добавлен плавный переход, добавлена интерактивность кнопкам

--- js/script.js
+++ js/script.js
@@ -1,17 +1,37 @@
+//меню гамбургер
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.nav-menu'),
-    menuItem = document.querySelectorAll('.nav-menu__list-item'),
-    hamburger = document.querySelector('.hamburger');
+        menuItem = document.querySelectorAll('.nav-menu__list-item'),
+        hamburger = document.querySelector('.hamburger');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
         menu.classList.toggle('nav-menu_active');
     });
 
-    menuItem.forEach(item => {
+    menuItem.forEach((item) => {
         item.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active');
             menu.classList.toggle('nav-menu_active');
-        })
-    })
-})
+        });
+    });
+});
+
+//плавный скролл
+function addSmoothScroll(anchor) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+        });
+    });
+}
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    addSmoothScroll(anchor);
+});
+
+//плавный переход с кнопки в первой секции "Записаться в группу"
+addSmoothScroll(document.querySelector('.btn-contacts'));
+//плавный переход с кнопки в третей секции "Записаться в группу"
+addSmoothScroll(document.querySelector('.btn-contacts2'));
